7073887b6ec060babd2572140b258b86e9adf809
steve Linz <steve.linz@gmail.com>
2020-11-12 08:47:46 -0600

    Criteria satisfied.

--- tabs.js
+++ tabs.js
@@ -1,26 +1,16 @@
 $(document).ready(
     function () {
         // event handlers
+
+        $("#totalTable").hide();
         $("button").click(totalCal);
+
         function totalCal() {
-            var meatCal,vegCal,sizeCal;
-
-            $("#meats").focusout(function() {
-                var sum = 0;
-                $('#meats :checkbox').click(function (string) {
-                    sum = 0;
-                    $('#meats :checkbox:checked').each(function(idx, elm) {
-                        sum += parseFloat(elm.value, 10);
-                        meatCal = meatCal + sum;
-                    });
-
-                    $('#meatReview').html(sum);
-                    $("#meatsReview").append(' ' + $('[name=meat]:checked').val());
-
-                });
-            });
-
-
+            var sizeCal;
+            var delivery = 2.00, tax = .051, total = 0;
+            var baconCal = 0, pepCal = 0, sausageCal = 0;
+            var tomatoesCal = 0, olivesCal = 0, mushroomCal = 0;
+            $("#totalTable").show();
 
             if (document.getElementById('small').checked) {
                 sizeCal = parseFloat(7.00);
@@ -32,65 +22,100 @@ $(document).ready(
                 sizeCal = parseFloat(12.00);
             }
 
-            if (document.getElementById('pepperoni').checked) {
-                meatCal = parseFloat(1.50);
-            }
-            if (document.getElementById('sausage').checked) {
-                meatCal = parseFloat(1.50);
-            }
-            if (document.getElementById('bacon').checked) {
-                meatCal = parseFloat(1.50);
-            }
-
             if (document.getElementById('tomatoes').checked) {
-                vegCal = parseFloat(1.00);
+                tomatoesCal = parseFloat(1.00);
             }
             if (document.getElementById('olives').checked) {
-                vegCal = parseFloat(1.00);
+                olivesCal = parseFloat(1.00);
             }
             if (document.getElementById('mushrooms').checked) {
-                vegCal = parseFloat(1.00);
+                mushroomCal = parseFloat(1.00);
             }
 
-            var preSubTotal = meatCal+vegCal+sizeCal;
+            if (document.getElementById('bacon').checked) {
+                baconCal = parseFloat(1.50);
+
+            }
+            if (document.getElementById('pepperoni').checked) {
+               pepCal = parseFloat(1.50);
+            }
+            if (document.getElementById('sausage').checked) {
+                sausageCal = parseFloat(1.50);
+            }
+
+            var preSubTotal = sizeCal + baconCal + pepCal + sausageCal +
+            tomatoesCal + olivesCal + mushroomCal;
             preSubTotal = parseFloat(preSubTotal);
-            $("#subTotal").text (preSubTotal.toFixed(2));
+            tax = tax * preSubTotal;
+            total = tax + delivery + preSubTotal;
+
+            $("#subTotal").text(preSubTotal.toFixed(2));
+            $("#delivery").text(delivery.toFixed(2));
+            $("#taxShow").text(tax.toFixed(2));
+            $("#totalAmount").text(total.toFixed(2));
         }
     });
 
-         $("#name").focusout(function() {
-            $("#nameReview").html($("#name").val());
-        });
+$("#name").focusout(function () {
+    $("#nameReview").html($("#name").val());
+});
 
-        $("#address").focusout(function() {
-            $("#addressReview").html($("#address").val());
-        });
+$("#address").focusout(function () {
+    $("#addressReview").html($("#address").val());
+});
 
-        $("#cell").focusout(function() {
-            $("#cellReview").html($("#cell").val());
-        });
-
-        $("#veggies").focusout(function() {
-            $("#veggiesReview").append(' ' + $('[name=veg]:checked').val());
-        });
-
-        $("#pizzaSize").focusout(function (){
-
-            $("#sizeReview").html($('[name=size]:checked').val());
-        });
-
-        $("#pizzaCrust").focusout(function (){
-
-            $("#crustReview").html($('[name=crust]:checked').val());
-        });
+$("#cell").focusout(function () {
+    $("#cellReview").html($("#cell").val());
+});
 
 
-        $("#tabs a").click(showTab);
+$("#pizzaSize").focusout(function () {
+
+    $("#sizeReview").html($('[name=size]:checked').val());
+});
+
+$("#pizzaCrust").focusout(function () {
+
+    $("#crustReview").html($('[name=crust]:checked').val());
+});
+
+$("#bacon").change(function () {
+
+    $("#meatsReview").append(" bacon ");
+});
+
+$("#pepperoni").change(function () {
+
+    $("#meatsReview").append(" pepperoni ");
+});
+
+$("#sausage").change(function () {
+
+    $("#meatsReview").append(" sausage ");
+});
+
+$("#tomatoes").change(function () {
+
+    $("#veggiesReview").append(" tomatoes ");
+});
+
+$("#olives").change(function () {
+
+    $("#veggiesReview").append(" olives ");
+});
+
+$("#mushrooms").change(function () {
+
+    $("#veggiesReview").append(" mushrooms ");
+});
 
 
-        // other functions
-        function showTab(event) {
-            event.preventDefault();
-            $(this).tab("show");
-        }
+$("#tabs a").click(showTab);
+
+
+// other functions
+function showTab(event) {
+    event.preventDefault();
+    $(this).tab("show");
+}
 
